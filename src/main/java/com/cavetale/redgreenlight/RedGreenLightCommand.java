b84cae3cf6eb0e59ac8ba181b7160bec160aafdd
StarTux <ths.loy@googlemail.com>
2022-07-10 15:49:06 +0200

    Implement Core AbstractCommand

--- src/main/java/com/cavetale/redgreenlight/RedGreenLightCommand.java
+++ src/main/java/com/cavetale/redgreenlight/RedGreenLightCommand.java
@@ -1,101 +1,107 @@
 package com.cavetale.redgreenlight;
 
-import java.util.List;
-import java.util.UUID;
-import lombok.RequiredArgsConstructor;
+import com.cavetale.core.command.AbstractCommand;
+import com.cavetale.core.command.CommandArgCompleter;
+import com.winthier.playercache.PlayerCache;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
-import org.bukkit.entity.Player;
+import org.bukkit.World;
+import static java.util.stream.Collectors.toList;
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
-@RequiredArgsConstructor
-public final class RedGreenLightCommand implements TabExecutor {
-    private final RedGreenLightPlugin plugin;
-
-    public void enable() {
-        plugin.getCommand("redgreenlight").setExecutor(this);
+public final class RedGreenLightCommand extends AbstractCommand<RedGreenLightPlugin> {
+    protected RedGreenLightCommand(final RedGreenLightPlugin plugin) {
+        super(plugin, "redgreenlight");
     }
 
     @Override
-    public boolean onCommand(final CommandSender sender, final Command command, final String alias, final String[] args) {
-        if (args.length == 0) return false;
-        switch (args[0]) {
-        case "load": {
-            plugin.load();
-            sender.sendMessage("RedGreenLight loaded");
-            return true;
-        }
-        case "setworld": {
-            if (args.length != 2) return false;
-            plugin.tag.world = args[1];
-            plugin.saveTag();
-            sender.sendMessage("World set to " + plugin.tag.world);
-            return true;
-        }
-        case "start": {
-            plugin.startTicking();
-            plugin.tag.started = true;
-            plugin.tag.ticks = 0;
-            plugin.tag.light = Light.RED;
-            plugin.tag.cooldown = 200;
-            plugin.saveTag();
-            sender.sendMessage("RedGreenLight started");
-            return true;
-        }
-        case "stop": {
-            plugin.stopTicking();
-            plugin.tag.started = false;
-            plugin.saveTag();
-            plugin.cleanUp();
-            plugin.tag.playing.clear();
-            sender.sendMessage("RedGreenLight stopped");
-            return true;
-        }
-        case "event": {
-            if (args.length > 2) return false;
-            if (args.length == 2) {
-                try {
-                    plugin.tag.event = Boolean.parseBoolean(args[1]);
-                } catch (IllegalArgumentException iae) {
-                    return false;
-                }
-                plugin.saveTag();
-            }
-            sender.sendMessage("Event mode: " + plugin.tag.event);
-            return true;
-        }
-        case "reset": {
-            plugin.tag.completions.clear();
-            plugin.saveTag();
-            plugin.computeHighscore();
-            sender.sendMessage(text("Completions were reset!", AQUA));
-            return true;
-        }
-        case "add": {
-            if (args.length != 3) return false;
-            Player target = Bukkit.getPlayerExact(args[1]);
-            int value = Integer.parseInt(args[2]);
-            UUID uuid = target.getUniqueId();
-            plugin.tag.addCompletions(uuid, value);
-            plugin.saveTag();
-            plugin.computeHighscore();
-            sender.sendMessage(text("Score of " + target.getName() + " is now " + plugin.tag.getCompletions(uuid), AQUA));
-            return true;
-        }
-        case "reward": {
-            int count = plugin.rewardHighscore();
-            sender.sendMessage(text("Rewarded " + count + " highscores", AQUA));
-            return true;
-        }
-        default: return false;
-        }
-    }
+    protected void onEnable() {
+        rootNode.addChild("load").denyTabCompletion()
+            .description("Reload the state")
+            .senderCaller(sender -> {
+                    plugin.load();
+                    sender.sendMessage("RedGreenLight loaded");
+                });
 
-    @Override
-    public List<String> onTabComplete(final CommandSender sender, final Command command, final String alias, final String[] args) {
-        return List.of("load", "start", "stop", "setworld", "event", "reset", "reward", "add", "reward");
+        rootNode.addChild("setworld").arguments("<world>")
+            .description("Set the game world")
+            .completers(CommandArgCompleter.supplyList(() -> Bukkit.getWorlds().stream()
+                                                       .map(World::getName)
+                                                       .collect(toList())))
+            .senderCaller((sender, args) -> {
+                    if (args.length != 1) return false;
+                    plugin.tag.world = args[0];
+                    plugin.saveTag();
+                    sender.sendMessage("World set to " + plugin.tag.world);
+                    return true;
+                });
+
+        rootNode.addChild("start").denyTabCompletion()
+            .description("Start the game")
+            .senderCaller(sender -> {
+                    plugin.startTicking();
+                    plugin.tag.started = true;
+                    plugin.tag.ticks = 0;
+                    plugin.tag.light = Light.RED;
+                    plugin.tag.cooldown = 200;
+                    plugin.saveTag();
+                    sender.sendMessage("RedGreenLight started");
+                });
+
+        rootNode.addChild("stop").denyTabCompletion()
+            .description("Stop the game")
+            .senderCaller(sender -> {
+                    plugin.stopTicking();
+                    plugin.tag.started = false;
+                    plugin.saveTag();
+                    plugin.cleanUp();
+                    plugin.tag.playing.clear();
+                    sender.sendMessage("RedGreenLight stopped");
+                });
+
+        rootNode.addChild("event").arguments("true|false")
+            .description("Set event state")
+            .completers(CommandArgCompleter.BOOLEAN)
+            .senderCaller((sender, args) -> {
+                    if (args.length > 1) return false;
+                    if (args.length == 1) {
+                        plugin.tag.event = CommandArgCompleter.requireBoolean(args[0]);
+                        plugin.saveTag();
+                    }
+                    sender.sendMessage("Event mode: " + plugin.tag.event);
+                    return true;
+                });
+
+        rootNode.addChild("reset").denyTabCompletion()
+            .description("Reset completions")
+            .senderCaller(sender -> {
+                    plugin.tag.completions.clear();
+                    plugin.saveTag();
+                    plugin.computeHighscore();
+                    sender.sendMessage(text("Completions were reset!", AQUA));
+                });
+
+        rootNode.addChild("add").arguments("<player> <score>")
+            .description("Add player highscore")
+            .completers(PlayerCache.NAME_COMPLETER,
+                        CommandArgCompleter.integer(i -> i != 0))
+            .senderCaller((sender, args) -> {
+                    if (args.length != 2) return false;
+                    PlayerCache target = PlayerCache.require(args[0]);
+                    int value = CommandArgCompleter.requireInt(args[1], i -> i != 0);
+                    plugin.tag.addCompletions(target.uuid, value);
+                    plugin.saveTag();
+                    plugin.computeHighscore();
+                    sender.sendMessage(text("Score of " + target.name + " is now "
+                                            + plugin.tag.getCompletions(target.uuid), AQUA));
+                    return true;
+                });
+
+        rootNode.addChild("reward").denyTabCompletion()
+            .description("Give highscore rewards")
+            .senderCaller(sender -> {
+                    int count = plugin.rewardHighscore();
+                    sender.sendMessage(text("Rewarded " + count + " highscores", AQUA));
+                });
     }
 }
